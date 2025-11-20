83a5e090dfb5591a3af8c84021c5b773357b54be
WoodWorker <cwiegert69@hotmail.com>
2025-11-19 19:07:57 -0500

    Auto-synced changes from parent directory

--- sync_Orca_git.sh
+++ sync_Orca_git.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # Source and destination directories
-SOURCE="/Users/corywiegert/Library/Application Support/OrcaSlicer/user/3054054929"
+#SOURCE="/Users/corywiegert/Library/Application Support/OrcaSlicer/user/3054054929"
+SOURCE="/Users/corywiegert/Library/Application Support/OrcaSlicer/user/default"
 DESTINATION="/Users/corywiegert/SynologyDrive/OrcaSlicer-Configurations"
 
 
